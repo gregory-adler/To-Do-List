970b4de71c7a446ca8e29d5372d527b36dfab7d3
Gregory Adler <greg7239@gmail.com>
2015-01-14 19:24:02 -0500

    To_Do_List
    To Do List that organizes items into three lists (now, soon, eventually)

---
+++ helper_methods.py
@@ -0,0 +1,270 @@
+from datetime import datetime
+import pickle as pickle 
+
+#global variable
+valid_ints= [0,1,2,3,4,5,6,7,8,9]  
+
+def calculate_days_remaining (due_date):  
+    difference= 0
+    index= []
+
+    for i in range (0, len(due_date)):
+        if due_date[i]=='/' or due_date[i]== '-':
+                index.append(i)
+        else:
+            try:
+                int (due_date[i])
+            except:
+                return False
+
+    try: 
+        month= int (due_date[0:index[0]])
+        day= int (due_date [index[0]+1:index[1]])
+        year= int (due_date [index[1]+1:])
+    except: 
+        return False
+
+    current_date= datetime.now()
+    current_month= int (current_date.month)
+    current_day= int (current_date.day)
+    current_year= int (current_date.year)
+
+    year_difference= year-current_year
+    month_difference= month- current_month
+    day_difference= day- current_day
+
+    #checks if earlier date than current
+    if month_difference==0:
+        if year_difference==0:
+            if day_difference<0: 
+                return "5000"
+            elif day_difference==0:
+                return 0
+    elif year_difference==0:
+        if month_difference<0:
+            return "5000"
+    if year_difference<0:
+        return "5000"
+
+    #main calculation
+    if year_difference==0:
+        pass
+    elif year_difference==1:
+        difference+= end_the_year(current_day, current_month, current_year, difference)
+        current_month=1
+        current_day= 1
+        current_year+= 1
+        month_difference= month-current_month
+        day_difference= day-current_day
+    else:
+        difference+= end_the_year(current_day, current_month, current_year, difference)
+        difference+= (year_difference-1)*365
+        #for leap year
+        difference+= 1
+        current_year= year
+        current_month=1
+        current_day= 1
+        day_difference= day-current_day
+        month_difference= month-current_month
+
+    if month_difference==0:
+        if day_difference <0:
+            return False
+        else:
+            difference+= day-current_day
+            return difference
+    else:
+        difference= find_difference_month(day, current_day, month, current_month, difference)
+        difference+= (day-1)
+        return difference
+
+
+
+
+def find_difference_month(day, current_day, month, current_month, difference):
+    days_in_each_month= [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    days_left= days_in_each_month[current_month]-current_day
+    difference+= days_left
+    current_month+=1
+    while (current_month!= month):
+        difference+=days_in_each_month[current_month]
+        current_month+=1
+    return difference
+
+def end_the_year (current_day, current_month, current_year, difference):
+    days_in_each_month= [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    days_left= days_in_each_month[current_month]-current_day
+    difference += days_left
+    current_month+=1
+    while (current_month!=13):
+        difference+= days_in_each_month[current_month]
+        current_month+=1
+    return difference
+
+
+def check_date_valid(due_date):
+    "returns True or False and days_remaining, returns string 'earlier date' if date is earlier than existing"
+
+    index= []
+    if len(due_date)>10 or len(due_date)<8:
+        return False
+
+    for i in range (0, len(due_date)):
+        if due_date[i]=='/' or due_date[i]== '-':
+                index.append(i)
+        else:
+            try:
+                int (due_date[i])
+            except:
+                return False
+
+    if len(index)!=2:
+        return False
+
+    try: 
+        month= int (due_date[0:index[0]])
+        day= int (due_date [index[0]+1:index[1]])
+        year= int (due_date [index[1]+1:])
+    except: 
+        return False
+
+    if month ==0 or month >12:
+        return False
+    if day==0 or day>31:
+        return False
+
+    try:
+        a= int(calculate_days_remaining(due_date))
+        if a==0:
+            pass
+        elif a== False:
+            return False
+        elif a== 5000:
+            return "earlier date"
+    except:
+        return False
+
+    return True
+
+def check_days_needed_valid (days_needed):
+    for i in range (0, len(days_needed)):
+        try: 
+            int (days_needed[i])
+        except:
+            return False
+
+    try:
+        if int(days_needed)>0:
+            pass
+        else:
+            return False
+    except:
+        return False
+
+
+def check_name_valid (name):
+    if len (name)==0:
+        return False
+
+    any_characters= False
+
+    for i in name:
+        if i== " ":
+            pass
+        else:
+            any_characters= True
+
+    return any_characters
+
+
+def append_days_left(to_do_list):
+    for i in range (0, len(to_do_list)):
+        a= calculate_days_remaining(to_do_list[i][1])
+        try: 
+            to_do_list[i][3]= a
+        except:
+            to_do_list[i].append(a)
+    return to_do_list
+
+def organize_list (to_do_list):
+    organized_list= []
+    now= []
+    soon= []
+    eventually= []
+
+    for i in range (0, len(to_do_list)):
+        if to_do_list[i][3]<= to_do_list[i][2]:
+            now.append(to_do_list[i])
+        elif to_do_list[i][2]==1:
+            if to_do_list[i][3]>1 and to_do_list[i][3]<=4:
+                soon.append(to_do_list[i])
+            else:
+                eventually.append(to_do_list[i])
+        elif to_do_list[i][2]>1 and to_do_list[i][2]<=4:
+            if to_do_list[i][3]>to_do_list[i][2] and to_do_list[i][3]<=5+(to_do_list[i][2]):
+                soon.append(to_do_list[i])
+            else:
+                eventually.append(to_do_list[i])
+        else:
+            if to_do_list[i][3]>to_do_list[i][2] and to_do_list[i][3]<=15:
+                soon.append(to_do_list[i])
+            else:
+                eventually.append(to_do_list[i])
+
+
+    #sort the list by days until due
+
+    now= sorted(now, key=lambda x:x[3])
+    soon= sorted(soon, key=lambda x:x[3])
+    eventually= sorted (eventually, key=lambda x:x[3])
+
+    organized_list.append(now)
+    organized_list.append(soon)
+    organized_list.append(eventually)
+
+
+    return organized_list
+
+def formatting_display (name, days_till_due):
+    #formatting= [spaces_needed, days_till_due]
+    a= len(name)
+    b= int(34-a)
+    less_than_ten= int(b-1)
+    less_than_hundred= int(b-2)
+    less_than_thousand= int(b-3)
+
+
+    if days_till_due==0:
+        formatting= [b* " ", "Today"]
+    elif days_till_due==1:
+        formatting= [b* " " , "1 day"]
+    elif days_till_due<10:
+        formatting= [less_than_ten* " ", str(days_till_due)+ " days"]
+    elif days_till_due<100:
+        formatting= [less_than_hundred * " ", str(days_till_due)+ " days"]
+    else:
+        formatting= [less_than_thousand * " ", str(days_till_due )+ " days"]
+    return formatting
+        
+
+
+
+def save_to_do(to_do_list): 
+    pickle.dump(to_do_list, open("user_data.txt", "wb"))
+
+def unsave_to_do():
+    try: 
+        return pickle.load(open("user_data.txt", "rb"))
+    except:
+        pass
+
+
+def save_specfile(slist, filename):
+    pickle.dump(slist, open(filename, "wb"))
+
+def unsave_specfile(filename):
+    try:
+        return pickle.load(open(filename, "rb"))
+    except: 
+        pass
+
---
+++ main.py
@@ -0,0 +1,509 @@
+
+try: 
+	from Tkinter import *
+except:
+	from tkinter import *
+from datetime import datetime
+import helper_methods
+
+from datetime import datetime
+import helper_methods
+
+#global variables
+
+to_do_list= []
+now_items= []
+soon_items= []
+eventually_items= []
+
+
+class GUI:
+	def __init__(self, root):
+		#Home Screen
+
+		frame= Frame(root)
+		frame.pack()
+
+		frame_two= Frame(root)
+		frame_two.pack()
+
+		frame_three= Frame(root)
+		frame_three.pack()
+
+		frame_four= Frame(root)
+		frame_four.pack()
+
+		frame_five= Frame(root)
+		frame_five.pack()
+
+		self.seperator_one= Label(frame)
+		self.seperator_one.pack()
+
+		self.title= Label (frame, text= "To Do List", width= 15, height=1, font= ("Helvetica", 52))
+		self.title.pack()
+
+
+
+		self.add_entry_button= Button(frame, text= "Add Entry", height= 1, font= ("TkDefaultFont", 12), command= lambda: self.make_entry_name(frame,to_do_list))
+		self.add_entry_button.config(height= 1, width= 9)
+		self.add_entry_button.pack()
+
+		self.seperator_two= Label(frame)
+		self.seperator_two.pack()
+
+
+		self.now_title= Label (frame, text= "Now", font=("Helvetica", 34))
+		self.now_title.pack()
+		self.scrollbar = Scrollbar(frame_two, orient=VERTICAL)
+		self.now_list= Listbox(frame_two, height= 9, width= 42, font= ("Courier"), yscrollcommand=self.scrollbar.set)
+		self.scrollbar.config(command=self.now_list.yview)
+		self.scrollbar.pack(side=RIGHT, fill=Y)
+		self.now_list.pack(fill=BOTH, expand=1)
+		self.now_list.pack()	
+
+		self.delete_now= Button(frame_three, text= "Clear Item", font= ("TkDefaultFont", 12), command= lambda now_list=self.now_list: self.delete_item_now(frame, now_list, to_do_list))
+		self.delete_now.config(height= 2, width= 8)
+		self.delete_now.pack()
+
+		self.seperator_three= Label(frame_three)
+		self.seperator_three.pack()
+
+		self.soon_title= Label (frame_three, text= "Soon", font=("Helvetica", 34))
+		self.soon_title.pack()
+		self.scrollbar_two = Scrollbar(frame_three, orient=VERTICAL)
+		self.soon_list= Listbox(frame_three, height= 9, width=42, font= ("Courier"), yscrollcommand=self.scrollbar_two.set)
+		self.scrollbar_two.config(command=self.soon_list.yview)
+		self.scrollbar_two.pack(side=RIGHT, fill=Y)
+		self.soon_list.pack(fill=BOTH, expand=1)
+		self.soon_list.pack()
+
+		self.delete_soon= Button(frame_four, text= "Clear Item", font= ("TkDefaultFont", 12), command= lambda soon_list=self.soon_list: self.delete_item_soon(frame, soon_list, to_do_list))
+		self.delete_soon.config(height= 2, width= 8)
+		self.delete_soon.pack()
+
+		self.seperator_four= Label(frame_four)
+		self.seperator_four.pack()
+
+		self.eventually_title= Label (frame_four, text= "Eventually", font=("Helvetica", 34))
+		self.eventually_title.pack()
+		self.scrollbar_three = Scrollbar(frame_four, orient=VERTICAL)
+		self.eventually_list= Listbox(frame_four, height= 9, width=42, font= ("Courier"), yscrollcommand=self.scrollbar_three.set)
+		self.scrollbar_three.config(command=self.eventually_list.yview)
+		self.scrollbar_three.pack(side=RIGHT, fill=Y)
+		self.eventually_list.pack(fill=BOTH, expand=1)
+		self.eventually_list.pack()
+
+		self.delete_eventually= Button(frame_five, text= "Clear Item", font= ("TkDefaultFont", 12), command= lambda eventually_list=self.eventually_list: self.delete_item_eventually(frame, eventually_list, to_do_list))
+		self.delete_eventually.config(height= 2, width= 8)
+		self.delete_eventually.pack()
+
+		#trys to load existing to_do_list
+		to_do_list= helper_methods.unsave_to_do()
+
+		try:
+			if len(to_do_list)!=0:
+				self.display_to_do_list(frame, to_do_list)
+		except:
+			to_do_list= []
+	
+
+	def delete_item_now(self, frame, list_delete, to_do_list):
+		a= int(list_delete.curselection()[0])
+		b= self.now_items[a]
+		to_do_list.remove(b)
+		list_delete.delete(ANCHOR)
+		helper_methods.save_to_do(to_do_list)
+		self.display_to_do_list(frame, to_do_list)
+
+
+	def delete_item_soon(self, frame, list_delete, to_do_list):
+		a= int(list_delete.curselection()[0])
+		b= self.soon_items[a]
+		to_do_list.remove(b)
+		list_delete.delete(ANCHOR)
+		helper_methods.save_to_do(to_do_list)
+		self.display_to_do_list(frame, to_do_list)
+
+	def delete_item_eventually(self, frame, list_delete, to_do_list):
+		a= int(list_delete.curselection()[0])
+		b= self.eventually_items[a]
+		to_do_list.remove(b)
+		list_delete.delete(ANCHOR)
+		helper_methods.save_to_do(to_do_list)
+		self.display_to_do_list(frame, to_do_list)
+
+
+
+	def make_entry_name (self, frame,to_do_list):
+
+		self.add_entry_button.pack_forget()
+
+		self.now_title.pack_forget()
+		self.now_list.pack_forget()
+		self.scrollbar.pack_forget()
+		self.delete_now.pack_forget()
+		self.seperator_three.pack_forget()
+
+		self.soon_title.pack_forget()
+		self.soon_list.pack_forget()
+		self.scrollbar_two.pack_forget()
+		self.delete_soon.pack_forget()
+		self.seperator_four.pack_forget()
+
+		self.eventually_title.pack_forget()
+		self.eventually_list.pack_forget()
+		self.scrollbar_three.pack_forget()
+		self.delete_eventually.pack_forget()
+
+
+
+		self.name_title= Label (frame, text= "Name of assignment", height= 2)
+		self.name_title.pack()
+
+		self.entry_text_box_name= Entry(frame)
+		self.entry_text_box_name.pack()
+
+		self.name_entry_add_button= Button (frame, text= "Enter", height=2, command= lambda: self.make_entry_date(frame, to_do_list))
+		self.name_entry_add_button.pack()
+
+	def make_entry_date (self, frame, to_do_list): 
+
+		#checks if valid name
+		name= self.entry_text_box_name.get()
+		check_valid_name= helper_methods.check_name_valid(name)
+
+		if check_valid_name== False:
+			try:
+				self.invalid_name.winfo_exists()
+				self.invalid_name.pack()
+			except:
+				self.invalid_name= Label(frame, text= "You didn't enter anything", fg="red" )
+				self.invalid_name.pack()
+		else: 
+			try:
+				self.invalid_name.winfo_exists()
+				self.invalid_name.pack_forget()
+			except: 
+				pass
+
+			self.name_title.pack_forget()
+			self.entry_text_box_name.pack_forget()
+			self.name_entry_add_button.pack_forget()
+			self.due_date_title= Label (frame, text= "When is the assignment due?", height=2)
+			self.due_date_title.pack()
+
+			self.tomorrow_button= Button (frame, text= "Tomorrow", height=2, command= lambda: self.add_tomorrow_item(frame,name, to_do_list))
+			self.tomorrow_button.pack()
+
+			self.or_title= Label (frame, text= "OR", height=2)
+			self.or_title.pack()
+
+			self.due_date_directions= Label (frame, text= "Enter date: mm/dd/yyyy", height=1)
+			self.due_date_directions.pack()
+
+			self.entry_text_box_one= Entry(frame)
+			self.entry_text_box_one.pack()
+
+			self.entry_add_button_one= Button (frame, text= "Enter", height=2, command= lambda: self.make_entry_days_needed(frame,name, to_do_list))
+			self.entry_add_button_one.pack()
+
+	def make_entry_days_needed (self, frame, name, to_do_list): 
+
+		#first checks if the date received is valid
+		due_date= self.entry_text_box_one.get()
+		check_valid_date= helper_methods.check_date_valid(due_date)
+
+		if check_valid_date== False:
+			#checks if other error label exists
+			try:
+				self.not_future_date.winfo_exists()
+				self.not_future_date.pack_forget()
+				try:
+					self.not_valid_date.winfo_exists()
+					self.not_valid_date.pack()
+
+				except:
+					self.not_valid_date= Label(frame, text= "Not valid, match formatting with instructions eg. 1/7/2015", fg="red" )
+					self.not_valid_date.pack()
+
+			#checks if relevant error label exists
+			except:
+				try:
+					self.not_valid_date.winfo_exists()
+				except:
+					self.not_valid_date= Label(frame, text= "Not valid, match formatting with instructions eg. 1/7/2015", fg="red" )
+					self.not_valid_date.pack()
+		elif check_valid_date== "earlier date":	
+
+			#checks if other error label exists
+			try:
+					self.not_valid_date.winfo_exists()
+					self.not_valid_date.pack_forget()
+					try:
+						self.not_future_date.winfo_exists()
+						self.not_future_date.pack()
+					except:
+						self.not_future_date= Label(frame, text= "Not valid, this date is in the past", fg="red" )
+						self.not_future_date.pack()
+
+			#checks if relevant error label exists			
+			except: 
+				try:
+					self.not_future_date.winfo_exists()
+				except:
+					self.not_future_date= Label(frame, text= "Not valid, this date is in the past", fg="red" )
+					self.not_future_date.pack()
+
+		#if date is valid, makes days_needed entry screen
+		else:
+			#removes error messages
+			try: 
+				self.not_valid_date.winfo_exists()
+				self.not_valid_date.pack_forget()
+			except:
+				pass
+
+			try:
+				self.not_future_date.winfo_exists()
+				self.not_future_date.pack_forget()
+			except:
+				pass
+
+			#makes new entry box
+
+			self.due_date_title.pack_forget()
+			self.tomorrow_button.pack_forget()
+			self.or_title.pack_forget()
+			self.entry_text_box_one.pack_forget()
+			self.due_date_directions.pack_forget()
+			self.entry_add_button_one.pack_forget()
+
+			self.days_needed_directions= Label (frame, text= "Days of work required: ", height=1)
+			self.days_needed_directions.pack()
+
+			self.entry_text_box_two= Entry(frame)
+			self.entry_text_box_two.pack()
+
+			self.entry_add_button_two= Button (frame, text= "Enter", height=1, command= lambda: self.entry_added(frame, name, due_date, to_do_list))
+			self.entry_add_button_two.pack()
+
+	def add_tomorrow_item (self, frame, name, to_do_list):
+		current_date= datetime.now()
+		month= int (current_date.month)
+		day= int (current_date.day)+1
+		year= int (current_date.year)
+
+		due_date= str(month)+ '/' +str(day) + '/' +str(year)
+		days_needed= 1
+
+    	#goes to homescreen
+		try: 
+			self.not_valid_date.winfo_exists()
+			self.not_valid_date.pack_forget()
+		except:
+			pass
+
+		try:
+			self.not_future_date.winfo_exists()
+			self.not_future_date.pack_forget()
+		except:
+			pass
+
+		#Returns to homescreen
+		self.due_date_title.pack_forget()
+		self.tomorrow_button.pack_forget()
+		self.or_title.pack_forget()
+		self.entry_text_box_one.pack_forget()
+		self.due_date_directions.pack_forget()
+		self.entry_add_button_one.pack_forget()
+		self.seperator_two.pack_forget()
+
+		self.add_entry_button.pack()
+		self.seperator_two.pack()
+
+
+		self.now_title.pack()
+		self.scrollbar.pack(side=RIGHT, fill=Y)
+		self.now_list.pack(fill=BOTH, expand=1)
+		self.now_list.pack()
+		self.delete_now.pack()
+		self.seperator_three.pack()
+
+		self.soon_title.pack()
+		self.scrollbar_two.pack(side=RIGHT, fill=Y)
+		self.soon_list.pack(fill=BOTH, expand=1)
+		self.soon_list.pack()
+		self.delete_soon.pack()
+		self.seperator_four.pack()
+
+		self.eventually_title.pack()
+		self.scrollbar_three.pack(side=RIGHT, fill=Y)
+		self.eventually_list.pack(fill=BOTH, expand=1)
+		self.eventually_list.pack()
+		self.delete_eventually.pack()
+
+		to_do_list.append([name, due_date, days_needed])
+
+		helper_methods.save_to_do(to_do_list)
+		
+		self.display_to_do_list(frame, to_do_list)
+
+
+		
+
+	def entry_added(self, frame, name, due_date, to_do_list): 
+
+		#checks if days_needed entry is valid
+		days_needed= self.entry_text_box_two.get()
+		check_days_needed= helper_methods.check_days_needed_valid(days_needed)
+
+		#if not valid
+		if check_days_needed== False:
+			try:
+				self.not_valid_days_needed.winfo_exists()
+			except:
+				self.not_valid_days_needed= Label(frame, text= "Not valid, not a number", fg="red" )
+				self.not_valid_days_needed.pack()
+
+		#if valid
+		else:
+
+			#removes errors
+			try:
+				self.not_valid_days_needed.winfo_exists()
+				self.not_valid_days_needed.pack_forget()
+			except:
+				pass
+
+			#saves variable
+			days_needed= int(days_needed)	
+
+			#returns to homescreen 
+			self.entry_text_box_two.pack_forget()
+			self.days_needed_directions.pack_forget()
+			self.entry_add_button_two.pack_forget()
+			self.seperator_two.pack_forget()
+
+			self.add_entry_button.pack()
+			self.seperator_two.pack()
+
+
+			self.now_title.pack()
+			self.scrollbar.pack(side=RIGHT, fill=Y)
+			self.now_list.pack(fill=BOTH, expand=1)
+			self.now_list.pack()
+			self.delete_now.pack()
+			self.seperator_three.pack()
+
+			self.soon_title.pack()
+			self.scrollbar_two.pack(side=RIGHT, fill=Y)
+			self.soon_list.pack(fill=BOTH, expand=1)
+			self.soon_list.pack()
+			self.delete_soon.pack()
+			self.seperator_four.pack()
+
+			self.eventually_title.pack()
+			self.scrollbar_three.pack(side=RIGHT, fill=Y)
+			self.eventually_list.pack(fill=BOTH, expand=1)
+			self.eventually_list.pack()
+			self.delete_eventually.pack()
+
+
+
+			index_date= []
+
+			#stores due_date in right format
+			for i in range (0, len(due_date)):
+				if due_date[i]=='/' or due_date[i]== '-':
+					index_date.append(i)
+			month= int (due_date[0:index_date[0]])
+			day= int (due_date [index_date[0]+1:index_date[1]])
+			year= int (due_date [index_date[1]+1:])
+			due_date= str(month) + '/' + str(day) + '/' + str(year)
+
+
+			to_do_list.append([name, due_date, days_needed])
+
+			helper_methods.save_to_do(to_do_list)
+
+
+			self.display_to_do_list(frame, to_do_list)
+
+	def display_to_do_list(self, frame, to_do_list):
+		#displays to_do_list
+
+		to_do_list= helper_methods.append_days_left(to_do_list)
+
+		organized_list= helper_methods.organize_list(to_do_list)
+
+		#delete old display
+		self.now_list.delete(0,END)
+		self.soon_list.delete(0, END)
+		self.eventually_list.delete(0,END)
+
+		#used for delete method
+		self.now_items= []
+		self.soon_items= []
+		self.eventually_items= []
+		count_now=1
+		count_soon=1
+		count_eventually=1
+
+		#Now display
+		for i in organized_list[0]:
+			if i[3]==5000:
+				to_do_list.remove(i)
+				continue
+			self.now_items.append(i)
+			formatting= helper_methods.formatting_display(i[0], i[3])
+			if len (i[0])>=34:
+				if i[3]<10:
+					i[0]= i[0][0:30]+ "... "
+				if i[3]>=10:
+					i[0]=i[0][0:29]+ "... "
+			self.now_list.insert (END, str(count_now)+"."+ i[0] +str(formatting[0]) +str(formatting[1]) )
+			count_now+=1
+
+
+		#Soon display
+		for i in organized_list[1]:
+			if i[3]==5000:
+				to_do_list.remove(i)
+				continue
+			self.soon_items.append(i)
+			formatting= helper_methods.formatting_display(i[0], i[3])
+			if len (i[0])>=34:
+				if i[3]<10:
+					i[0]= i[0][0:29]+ "... "
+				if i[3]>=10:
+					i[0]=i[0][0:28]+ "... "
+			self.soon_list.insert (END, str(count_soon)+ "." + i[0] +formatting[0] +formatting[1] )
+			count_soon+=1
+
+		#Eventually display
+		for i in organized_list[2]:
+			if i[3]==5000:
+				to_do_list.remove(i)
+				continue
+			self.eventually_items.append(i)
+			formatting= helper_methods.formatting_display(i[0], i[3])
+			if len (i[0])>=34:
+				if i[3]<10:
+					i[0]= i[0][0:29]+ "... "
+				if i[3]>=10:
+					i[0]=i[0][0:28]+ "... "
+			self.eventually_list.insert (END, str(count_eventually)+ "." + i[0] +formatting[0] +formatting[1] )
+			count_eventually+=1
+
+
+
+
+
+
+#variable declaration
+root= Tk()
+GUI= GUI(root)
+root.geometry('400x830')
+
+#program loop
+root.mainloop()
